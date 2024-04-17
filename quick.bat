37c22b8a153cedb5ce09a89aa303290e7353234b
Cunninger <1803912219@qq.com>
2024-04-17 21:18:37 +0800

    JVM堆区

---
+++ quick.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001
+REM 切换到你的项目目录
+cd D:\B_All\Java_八股_personal\TangSongYuanPoem
+
+REM 添加所有改动到暂存区
+git add .
+
+REM 提交改动
+git commit -m "JVM堆区"
+
+REM 推送改动到远程仓库
+git push origin master
